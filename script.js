4e2fa1e11927636fb1ecc2b707d7858e621b4d7b
toosloe <chloemarileejones@gmail.com>
2025-03-26 00:32:22 -0400

    Used JS to switch door images rather than having them on top of eachother. Works better and doesnt mess with positioning.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
 const splash = document.getElementsByClassName("splash")[0];
+const closedDoor = document.getElementsByClassName("door1")[0];
+
+function hideDoor(){
+    closedDoor.addEventListener("mouseover", (event) => {
+        closedDoor.src="images/Door2.PNG";
+        closedDoor.style.cursor="grab";
+    })
+
+    closedDoor.addEventListener("mouseleave", (event) => {
+        closedDoor.src="images/Door1.PNG";
+        closedDoor.style.cursor="pointer";
+    })
+
+    closedDoor.addEventListener("click", (event) => {
+        closedDoor.style.cursor="grabbing";
+    })
+}
 
 function hideSplash() {
     
@@ -7,8 +24,9 @@ function hideSplash() {
         splash.style.opacity = "0%";
         splash.style.animationName = "fadeOut";
         splash.style.animationDuration = "1s";
-        splash.style.zIndex = "-1";
+        splash.style.zIndex = "-3";
     });
 }
 
+hideDoor();
 hideSplash();
